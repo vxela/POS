934b1b660da74394fa84033f334fc1957359b7bd
mush60 <mushti8@gmail.com>
2019-10-17 02:17:15 +0700

    get data kategori and data user from produk model pass it in produk view

--- app/Models/Tbl_product.php
+++ app/Models/Tbl_product.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Tbl_product extends Model
 {
     //
+
+    public function category() {
+        return Tbl_product_categorie::where('id', $this->category_product_id)->first();
+    }
+
+    public function getUser() {
+        return \App\User::where('id', $this->user_id)->first();
+    }
 }
