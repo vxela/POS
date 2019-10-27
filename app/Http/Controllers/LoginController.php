a64a8dcc5e16474f0b58b4f259c5918de026fc6a
mush60 <mushti8@gmail.com>
2019-10-27 22:05:14 +0700

    fixing session problem

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Session;
 
 class LoginController extends Controller
 {
@@ -39,6 +40,7 @@ class LoginController extends Controller
 
     public function logout(){
         Auth::logout();
+        Session::forget('transKey');
         return redirect('/login');
     }
 }
