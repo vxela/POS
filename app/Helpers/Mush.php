f106f81f8e4f6679c751c8975249a4d91a193725
mush60 <mushti8@gmail.com>
2019-11-01 08:35:45 +0700

    fixxing helper Mush, eror substring at windows

--- app/Helpers/Mush.php
+++ app/Helpers/Mush.php
@@ -7,30 +7,19 @@ class Mush {
 
     public static function getUniqStr() {
 
-        return substr(
-            base_convert(
-                sha1(
-                    uniqid(
-                        mt_rand()
-                    )
-                ), 16, 36
-            ), 0, str_replace(
-                ".", "", 
-                str_replace(
-                    " ", "", 
-                    microtime()
-                )
-            )
-        );
+        $strUniq = md5(str_replace(".", "", str_replace(" ", "", microtime())).base_convert(sha1(uniqid(mt_rand())), 16, 36));
 
-        // usage :
-        // Mush::getUniqStr()
+        return $strUniq;
+
     }
-
+    
     public static function showNotif($status, $msg){
         echo "<div id='m_notif' data-flash_type='".$status."' data-flash_msg='".$msg."'>
         </div>";
     }
-
+    
     
 }
+
+// usage :
+// Mush::getUniqStr()
